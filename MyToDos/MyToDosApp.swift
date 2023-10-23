81d35c9e40874bef72e1e5f8b8ba949591842e60
Vladimir Fibe <motiw@icloud.com>
2023-10-23 21:39:59 +0600

    Initial Commit

---
+++ MyToDos/MyToDosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDosApp.swift
+//  MyToDos
+//
+//  Created by Vladimir Fibe on 23.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyToDosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
